30a2fbcca96c84a4f01d6588aada4c8729e4e285
Avril Gill <avrilgill@outlook.com>
2021-05-03 12:06:03 -0700

    rebuild project

--- src/main/java/com/example/imagevault/ImageRepositoryPackage/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.imagevault.ImageRepositoryPackage;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ImageRepository extends MongoRepository<Image, String> {
-
-}
