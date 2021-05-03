011bb3f723c04ec3bb48197d0c9784b566551ff9
Avril Gill <avrilgill@outlook.com>
2021-05-03 12:18:37 -0700

    upload file syste,

--- src/main/java/com/example/imagevault/ImageVaultApplication.java
+++ src/main/java/com/example/imagevault/ImageVaultApplication.java
@@ -1,13 +1,27 @@
 package com.example.imagevault;
 
+import com.example.imagevault.Storage.StorageProperties;
+import com.example.imagevault.Storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ImageVaultApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImageVaultApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
